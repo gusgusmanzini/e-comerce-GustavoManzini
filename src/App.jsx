db7aa1f658d4d0b64766a54bf410ba22dc2ebdf0
Gustavo <tavitooo14@icloud.com>
2024-07-26 15:45:17 -0300

    Agregue Link a nosotros con info

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import NavBar from "./components/Navbar.jsx";
 import ItemListContainer from "./components/ItemListContainer.jsx";
 import ItemDetailContainer from "./components/ItemDetailContainer.jsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Nosotros from "./components/Nosotros.jsx";
+import Nosotros from "./components/Nosotros";
 
 function App() {
   return (
@@ -16,6 +16,7 @@ function App() {
           <Route path="/" element={<ItemListContainer />} />
           <Route path="category/:id" element={ <ItemListContainer  />} />
           <Route path="/item/:id" element={<ItemDetailContainer />} />
+          <Route path="/nosotros" element={<Nosotros />}  />
         </Routes>
 
 
--- src/components/Nosotros.jsx
+++ src/components/Nosotros.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
-export default function Nosotros() {
+const Nosotros = () => {
   return (
-    <div>
-      <h1>holaa</h1>
+    <div className="nosotros">
+      <h1 className="n-title">Tienda Mates</h1>
+      <p className="p-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus corrupti, non molestias exercitationem nulla aliquid alias, iste distinctio similique in mollitia dolorem vitae quibusdam adipisci inventore voluptatum enim quidem totam dignissimos beatae eaque amet repellat voluptatem? Incidunt doloremque minus tenetur. Id accusantium soluta saepe laboriosam ratione. Incidunt sapiente, sed praesentium quis quae ratione repellendus, quisquam minima, velit doloribus voluptates. Numquam natus rerum velit debitis quaerat cupiditate dolorem impedit temporibus vel dolore magni perferendis, blanditiis aperiam eum voluptas laudantium sequi reprehenderit suscipit illo commodi minima deserunt? Tenetur quis labore optio nihil tempora laborum rerum iste cumque iusto. Voluptatum quis tempore expedita unde quibusdam error ipsa magnam facilis rem aliquam temporibus, nisi, aperiam delectus officiis laboriosam nobis? Delectus, tempore? Quo molestiae voluptatibus rem doloremque repellendus alias? Assumenda animi quo voluptas cumque ea? Fugit quo eum nulla sint nihil esse expedita rerum nobis accusantium facere sequi hic, eaque officiis pariatur facilis dignissimos ut!</p>
     </div>
   )
 }
+
+export default Nosotros
